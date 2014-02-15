26a78303de038cdc8f449e9900d670c6bf1c51d3
Andy Cohan <andy@cohan.org>
2014-02-15 11:14:49 -0500

    added config.fish

---
+++ config.fish
@@ -0,0 +1,13 @@
+set GROOVY_HOME /Applications/groovy-1.8.6
+set PATH $PATH $GROOVY_HOME/bin
+set GRAILS_HOME /Applications/grails-2.1.0/
+set PATH $PATH $GRAILS_HOME/bin
+set GLASSFISH_HOME /usr/local/Cellar/glassfish/3.1.2
+set PATH $PATH $GLASSFISH_HOME/bin
+set HISTSIZE 5000
+set HISTFILESIZE 5000
+set EDITOR vim
+set VISUAL vim
+set BROWSER open
+alias lt 'ls -lt'
+alias gs 'git status'
